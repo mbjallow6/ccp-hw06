dcdee083637ff93db53d615cc5b3047308d75209
Jallow_加林 <bailo.jallow@yahoo.com>
2023-12-28 03:24:15 +0100

    created a new app experiments

---
+++ experiments/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Experiment
+
+def index(request):
+    experiments = Experiment.objects.all()
+    return render(request, 'experiments/index.html', {'experiments': experiments})
+
+def detail(request, experiment_id):
+    experiment = Experiment.objects.get(pk=experiment_id)
+    return render(request, 'experiments/detail.html', {'experiment': experiment})
